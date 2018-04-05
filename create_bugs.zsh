a420b466e4612afd3d55f2326bdf6439951331dc
mindonly <thesanch@gmail.com>
2018-04-04 20:35:04 -0400

    baseline working ok without validations.

---
+++ create_bugs.zsh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+
+http :3000/users/1/bugs title='toolbelt maintenance' description='this thing does not work'
+http :3000/users/2/bugs title='cape maintenance' description='this thing does not fly'
+http :3000/users/3/bugs title='katana maintenance' description='this thing is dull'
---
+++ create_users.zsh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+
+http :3000/users fname='Bruce' lname='Wayne' email='bw@batcave.net' thumbnail='batman.png'
+http :3000/users fname='Clark' lname='Kent' email='ck@dailyplanet.net' thumbnail='superman.gif'
+http :3000/users fname='Wade' lname='Wilson' email='ww@deadpool.net' thumbnail='merc.jpg'
