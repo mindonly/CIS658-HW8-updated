5756f43c75b5d188ccb878b7dbdea9ce8499a7ab
Rob Sanchez <thesanch@gmail.com>
2018-04-05 13:48:54 -0400

    add create scripts.

--- create_bugs.zsh
+++ create_bugs.zsh
@@ -1,5 +1,12 @@
 #!/usr/local/bin/zsh
 
+# create bugs
 http :3000/users/1/bugs title='toolbelt maintenance' description='this thing does not work'
 http :3000/users/2/bugs title='cape maintenance' description='this thing does not fly'
 http :3000/users/3/bugs title='katana maintenance' description='this thing is dull'
+
+# change priority of user 2 bug 2
+http PATCH :3000/users/2/bugs/2 priority='high'
+
+# change issue_type for user 2 bug 2
+http PATCH :3000/users/2/bugs/2 issue_type='enhancement'
--- create_users.zsh
+++ create_users.zsh
@@ -1,5 +1,13 @@
 #!/usr/local/bin/zsh
 
+# add users
 http :3000/users fname='Bruce' lname='Wayne' email='bw@batcave.net' thumbnail='batman.png'
 http :3000/users fname='Clark' lname='Kent' email='ck@dailyplanet.net' thumbnail='superman.gif'
+http :3000/users fname='Peter' lname='Parker' email='pp@nyc.net' thumbnail='spidey.png'
 http :3000/users fname='Wade' lname='Wilson' email='ww@deadpool.net' thumbnail='merc.jpg'
+
+# modify Clark Kent's email
+http PATCH :3000/users/2 email='ck@metropolis.net'
+
+# delete Peter Parker
+http DELETE :3000/users/3
